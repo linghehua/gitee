e69fa4c22f9668565405cf6e4e77780c39680c2e
mywin8 <win8@139.com>
2019-09-09 16:59:17 +0800

    2019年9月9日 学习pycharm版本控制第4次提交！

--- mygit.py
+++ mygit.py
@@ -2,13 +2,20 @@
 # -*- coding: utf-8 -*- 
 
 def main():
-    PI = 3.141592648
-    print('PI is :%s',PI)
-    list1 = ['china','second','tom']
-    list1.insert(1,'hello')
-    print(list1)
-    
-    
+    height = 1.75
+    weight = 80.5
+    bmi = weight / (pow(height, 2))
+    if bmi < 18.5:
+        print('BMI is{} ,过轻'.format(bmi))
+    elif bmi < 25:
+        print('BMI is {},正常'.format(bmi))
+    elif bmi < 28:
+        print('BMI is {},过重'.format(bmi))
+    elif bmi < 32:
+        print('BMI is {},肥胖'.format(bmi))
+    else:
+        print('BMI is {},严重肥胖'.format(bmi))
+        
 
 if __name__ == '__main__':
     main()
